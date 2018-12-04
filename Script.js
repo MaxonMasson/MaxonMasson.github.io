43357a2ec7a344fff2876482aa32ff22357839de
Maxim <mrdada39@gmail.com>
2018-12-04 11:41:33 +0300

    Hubby

---
+++ Script.js
@@ -0,0 +1,18 @@
+﻿jQuery('document').ready(function(){
+  
+    jQuery('button').on('click',function(){
+	  var value1, value2, value3;
+	  value1 = jQuery('#One').val();
+	  value2 = jQuery('#Two').val();
+	  value1 = parseInt(value1);
+	  value2 = parseInt(value2);
+	  value3 = value1 + value2;
+	  jQuery('#Res').html(value3);
+    });
+   var Nickname;
+   
+   Nickname = prompt('Введите ваш никнейм','')
+   jQuery('#Nick').html(Nickname);
+
+});
+
